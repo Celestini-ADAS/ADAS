0d9774d668c19e51907efc101602c81cb94fa302
Celestini-Lucifer <39214038+Celestini-Lucifer@users.noreply.github.com>
2018-07-20 12:23:39 +0530

    Update COMS.py

--- COMS.py
+++ COMS.py
@@ -15,15 +15,19 @@ class com_handler:
 		self.ser.close()
 		self.ser.close()
 
+	def clear(self):
+		self.ser.flush()
+
 	def send(self,info_Tx):
 		message_Tx = info_Tx.tobytes()
 		self.ser.write(message_Tx)
 
 	def check_recv(self):
-		return self.ser.inWaiting()==self.number_of_bytes  
+		return self.ser.inWaiting()>=self.number_of_bytes  
 	def read(self):
 		message_Rx = self.ser.read(self.number_of_bytes)
 		info_Rx = np.frombuffer(message_Rx,dtype = float)
+		self.ser.flush()# this is to make sure that the data is always new. 
 		return info_Rx
 
 
