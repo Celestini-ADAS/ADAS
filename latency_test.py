cb91e5aed5cda52df0aa6398b2787f117f0929cb
Celestini-Lucifer <39214038+Celestini-Lucifer@users.noreply.github.com>
2018-07-21 11:31:36 +0530

    Update latency_test.py

--- latency_test.py
+++ latency_test.py
@@ -4,16 +4,16 @@ import time
 
 number = 40
 
-Tx = com_handler()
+Tx = com_handler() #create object
 Rx = com_handler()
 
-Tx._init_('COM8',9600,number)
+Tx._init_('COM8',9600,number) #initialize com port with baud rate and number of bytes in a single packet
 Rx._init_('COM9',9600,number)
 
-info_Tx = np.ones(5)
+info_Tx = np.ones(5) #info to transfer
 time_Stamp = time.time()
 
-Tx.send(info_Tx)
+Tx.send(info_Tx) # sending info
 while(not Rx.check_recv()):
 	pass
 
