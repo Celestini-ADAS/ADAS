16d5ddc4bd3987ff83a49e5a3b0714fd30892a06
Celestini-Lucifer <39214038+Celestini-Lucifer@users.noreply.github.com>
2018-07-24 11:56:38 +0530

    Update ADAS.py

--- ADAS.py
+++ ADAS.py
@@ -7,25 +7,26 @@ from COMS import * # same
 
 #initialize coms
 V2V = com_handler()
-V2V._init_('COM8',230400,40)
+V2V._init_('COM4',230400,40)
 
 phone = com_handler()
-phone.initialize('COM?',BAUD,48)
-
-cap = cv2.videoCapture(0)
-#initialize
-ret,img = cap.read()
+phone.initialize('dev/tty/USB0',250000,48)
+cap = np.load('Test1.npy') # test video stored in numpy format because opencv in raspi didn't work with .MOV files.
+#cap = cv2.VideoCapture('Test2.MOV') 
+#initialize stuff for vision 
+img = cap[0][0]
 prev_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 broadcast_Rx = np.zeros(5)
 
-state = np.zeros(6) # state array. lon,lat,speed,direction,time_stamp(RTC),acceleration along direction of movement.
-
+# state = np.zeros(6) # state array. lon,lat,speed,direction,time_stamp(RTC),acceleration along direction of movement.
+state = np.array([10,10,15,45,0,1],dtype=float) # static coordinates for testing purposes
 def check_Relevance(state,broadcast_Rx):
 	L=2
-
-	A_X = -m.cos(state[3])
-	A_Y = -m.sin(state[3])
+	if((state[4]-broadcast_Rx[4])>10):
+		return 0
+	A_X = -m.cos(state[3]*0.01745)
+	A_Y = -m.sin(state[3]*0.01745)
 
 	Xr = state[0]
 	Xs = broadcast_Rx[0]
@@ -34,7 +35,7 @@ def check_Relevance(state,broadcast_Rx):
 	dX = Xr-Xs
 	dY = Yr-Ys
 	D = m.sqrt(dX**2 + dY**2)
-	Ms = tan(state[3])
+	Ms = m.tan(state[3]*0.01745)
 	Mr = dY/dX
 	T = (Mr - Ms)/(1 + Mr*Ms)
 	if(A_X*dX>0 and A_Y*dY>0):
@@ -43,23 +44,63 @@ def check_Relevance(state,broadcast_Rx):
 			return 1
 	return 0
 
-while True:
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText_Center = (150,200)
+bottomLeftCornerOfText_Left = (150,300)
+bottomLeftCornerOfText_Right = (150,100)
+fontScale              = 1
+fontColor              = (0,0,255)
+lineType               = 2
+
+while 1:
 	
-	if(phone.check_recv()): #if new information from phone has been received
-		state = phone.read() #update the state
+	# if(phone.check_recv()): #if new information from phone has been received
+	# 	state = phone.read() #update the state
 	
 	if(V2V.check_recv()):#if an alert has arrived, 
 		broadcast_Rx = V2V.read()#get the alert data
 		if(check_Relevance(state,broadcast_Rx)):
-			give_user_alert_somehow()
+			print('ALEEEERRTTTTTT!!!!')
 
-	ret,img = cap.read()
+	img = cap[i][0]
 	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	alert = check(img,gray,prev_gray)
 	prev_gray = gray
-	if(alert):
-		give_user_alert_somehow() #define this ples
-		if(alert>some_threshold or state[5]<-7): #if optical flow threat is too great or deceleration is more than 7m/s^	
-			V2V.send(state[:-1])
+	
+	if(alert[1]):
+		cv2.putText(img,'alert_center!', 
+		bottomLeftCornerOfText_Center, 
+		font, 
+		fontScale,
+		fontColor,
+		lineType)
+		cv2.imshow('window',img)
+	if(alert[0]):
+	    cv2.putText(img,'alert_left!', 
+	    bottomLeftCornerOfText_Left, 
+	    font, 
+	    fontScale,
+	    fontColor,
+	    lineType)
+	    cv2.imshow('window',img)
+	if(alert[2]):
+	    cv2.putText(img,'alert_right!', 
+	    bottomLeftCornerOfText_Right, 
+	    font, 
+	    fontScale,
+	    fontColor,
+	    lineType)
+	    cv2.imshow('window',img)
+	else:
+	    cv2.imshow('window',img)
 
 
+	if(alert.any()):
+		print('ALEEEERRTTTTTT!!!!') #define this ples
+		if(alert.any() or state[5]<(-7)): #if optical flow threat is too great or deceleration is more than 7m/s^	
+			V2V.send(state[:-1]) # send everything except the last info which is the deceleration because we can only broadcast 40 bytes at a time(For now)
+
+	ch = 0xFF & cv2.waitKey(5) #press ESC key to exit
+	if ch == 27:
+	    break
+cv2.destroyAllWindows() 	
